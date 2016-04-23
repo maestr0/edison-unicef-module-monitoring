45dc4fbe446f4ab81b99c97bf1a6802a369d96d3
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-04-23 00:42:02 -0400

    fixed dependencies

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Monitoring');
+    res.send('Monitoring');
+});
+
+app.get('/status', function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    //FIXME: implement status
+    var device = req.query.device;
+    res.send({
+        "status": "ok"
+    });
 });
 
 app.set('port', (process.env.MONITORING_PORT || 3001));
 
 app.listen(app.get('port'), function () {
-  console.log('Monitoring listening on port ' + app.get('port'));
+    console.log('Monitoring listening on port ' + app.get('port'));
 });
 
 
@@ -31,20 +42,18 @@ process.env.REBOOT_COUNT;
 
 
 
-
-
 // "q": "latest",
 // "jsupm_mpr121": "latest"
 console.log("starting monitoring app");
-var gyroRunLoopInterval   = 500 ; // in milliseconds
-var soapRunLoopInterval   = 200 ; // in milliseconds
+var gyroRunLoopInterval = 500; // in milliseconds
+var soapRunLoopInterval = 200; // in milliseconds
 
-var gyroZaxisTransient     = 0x20 ; //0o00100000 ;/
-var gyroZaxisLatchedHigh   = 0x60 ; //01100000 ;
-var gyroZaxisLatchedBoth   = 0x70 ; //01100000 ;
-var gyroInterruptActive    = 0x80 ; // 10000000 ;
-var weAreRotating          = 0x60 ; //0x40 is interrupt triggered
-var touchThresholdAddress  = 0x41 ; // address to set the touch threshold, 
+var gyroZaxisTransient = 0x20; //0o00100000 ;/
+var gyroZaxisLatchedHigh = 0x60; //01100000 ;
+var gyroZaxisLatchedBoth = 0x70; //01100000 ;
+var gyroInterruptActive = 0x80; // 10000000 ;
+var weAreRotating = 0x60; //0x40 is interrupt triggered
+var touchThresholdAddress = 0x41; // address to set the touch threshold,
 
 var mraa = require('mraa');
 
@@ -55,14 +64,14 @@ touchInterruptPin.isr(mraa.EDGE_BOTH, irqCallback);
 
 var touchSensorDriver = require('jsupm_mpr121');
 var myTouchSensor = new touchSensorDriver.MPR121(touchSensorDriver.MPR121_I2C_BUS, touchSensorDriver.MPR121_DEFAULT_I2C_ADDR);
-myTouchSensor.configAN3944();
+//myTouchSensor.configAN3944();
 var buffer = [128];
 var oneByte = new Number(1);
 
 // --- Use wire here since UPM is --------------------- //
-var i2c = new mraa.I2c(touchSensorDriver.MPR121_I2C_BUS);  
-i2c.address(touchSensorDriver.MPR121_DEFAULT_I2C_ADDR);  
-i2c.writeReg(0x41,255);
+var i2c = new mraa.I2c(touchSensorDriver.MPR121_I2C_BUS);
+i2c.address(touchSensorDriver.MPR121_DEFAULT_I2C_ADDR);
+i2c.writeReg(0x41, 255);
 
 
 //Pin setup for gyroscope interrupt
@@ -71,14 +80,13 @@ i2c.writeReg(0x41,255);
 //gyroInterruptPin.isr(mraa.EDGE_BOTH, gyroInterruptCallBack);
 
 
-var IMUClass         = require('jsupm_lsm9ds0');  // Instantiate an LSM9DS0 using default parameters (bus 1, gyro addr 6b, xm addr 1d)
-var gyroAccelCompass = new IMUClass.LSM9DS0()  ;
+var IMUClass = require('jsupm_lsm9ds0'); // Instantiate an LSM9DS0 using default parameters (bus 1, gyro addr 6b, xm addr 1d)
+var gyroAccelCompass = new IMUClass.LSM9DS0();
 
-if ( isEmpty(gyroAccelCompass)){
+if (isEmpty(gyroAccelCompass)) {
     console.error("NO GYROSCOPE");
-}
-else {
-    gyroAccelCompass.init();                          // Initialize the device with default values
+} else {
+    gyroAccelCompass.init(); // Initialize the device with default values
     setupGyroscope();
 }
 
@@ -119,12 +127,12 @@ var processLogQueue = function () {
 var takingPictures = false;
 var exec = require('child_process').exec;
 
-var createPackagePrefix = function(){
+var createPackagePrefix = function () {
     // TODO: implement this shit
     var randomPart = Math.random().toString(36).substring(10);
     var rebootCount = "FIXME"; // process.env.REBOOT_COUNT  file path
     var datetime = "2016_06_01_134501"; // FIXME
-    return rebootCount + "_" + datetime +"_" + randomPart;
+    return rebootCount + "_" + datetime + "_" + randomPart;
 }
 
 var takePicture = function () {
@@ -141,8 +149,8 @@ var takePicture = function () {
 
 
 
-//    HERE CALL   
-process.env.SCRIPTS + "/archive.sh " + prefix;
+            //    HERE CALL
+            process.env.SCRIPTS + "/archive.sh " + prefix;
 
 
 
@@ -176,7 +184,7 @@ process.env.SCRIPTS + "/archive.sh " + prefix;
 setInterval(function () {
 
     if (soapHasBeenTouched()) {
-         console.log("Button pressed @ " + new Date().getTime());
+        console.log("Button pressed @ " + new Date().getTime());
         //queue.push("button pressed @ " + new Date().getTime());
     }
 }, soapRunLoopInterval);
@@ -185,18 +193,18 @@ setInterval(function () {
 
 //------- WATER CONTAINER ROTATION
 setInterval(function () {
-    
-    if ( thereIsARotation()) {
-        console.log( "Rotation detected");
+
+    if (thereIsARotation()) {
+        console.log("Rotation detected");
         gyroAccelCompass.update();
-        
+
         var x = new IMUClass.new_floatp();
         var y = new IMUClass.new_floatp();
         var z = new IMUClass.new_floatp();
         gyroAccelCompass.getGyroscope(x, y, z);
-        var gyroData = "Gyroscope:     GX: " + Math.round(IMUClass.floatp_value(x)) + " AY: " + Math.round(IMUClass.floatp_value(y)) +" AZ: " + Math.round(IMUClass.floatp_value(z)) ;
+        var gyroData = "Gyroscope:     GX: " + Math.round(IMUClass.floatp_value(x)) + " AY: " + Math.round(IMUClass.floatp_value(y)) + " AZ: " + Math.round(IMUClass.floatp_value(z));
         console.log(gyroData);
-        
+
         if (!takingPictures) {
             takingPictures = true;
             takePicture();
@@ -215,7 +223,7 @@ function irqCallback() {
     // console.log("IRQ callback() " + new Date().getTime());
 }
 
-function gyroInterruptCallBack(){    
+function gyroInterruptCallBack() {
     // Leave this callback empty, only for waking up the device
     //console.log("gyroIRQ callback " + new Date().getTime());
 }
@@ -230,39 +238,39 @@ function soapHasBeenTouched() {
     return (myTouchSensor.m_buttonStates & 1);
 }
 
-function setupGyroscope(){
-gyroAccelCompass.writeReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_INT1_CFG_G,  0x60 );
-gyroAccelCompass.writeReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG1_G, 0x0F );
-gyroAccelCompass.writeReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG2_G, 0x00 );
-gyroAccelCompass.writeReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG3_G, 0x88 );
-gyroAccelCompass.writeReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG5_G, 0x00 );
+function setupGyroscope() {
+    gyroAccelCompass.writeReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_INT1_CFG_G, 0x60);
+    gyroAccelCompass.writeReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG1_G, 0x0F);
+    gyroAccelCompass.writeReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG2_G, 0x00);
+    gyroAccelCompass.writeReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG3_G, 0x88);
+    gyroAccelCompass.writeReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG5_G, 0x00);
 
-gyroAccelCompass.writeReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_INT1_TSH_ZH_G, 0x25 ); //set threshold for high rotation speed
+    gyroAccelCompass.writeReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_INT1_TSH_ZH_G, 0x25); //set threshold for high rotation speed
 
 
 
 }
 
-function thereIsARotation(){
-    if (gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_INT1_SRC_G ) >= weAreRotating ) return true;
+function thereIsARotation() {
+    if (gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_INT1_SRC_G) >= weAreRotating) return true;
     return false;
 }
 
-function showGyrodebugInfo(){
-    console.log( "Gyro REG1: 0x" + gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG1_G ).toString(16) );
-    console.log( "Gyro REG2: 0x" + gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG2_G ).toString(16) );
-    console.log( "Gyro REG3: 0x" + gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG3_G ).toString(16) );
-    console.log( "Gyro REG4: 0x" + gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG4_G ).toString(16) );
-    console.log( "Gyro REG5: 0x" + gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_CTRL_REG5_G ).toString(16) );
-    console.log( "Gyro status" + gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_STATUS_REG_G ).toString(16) );
-    console.log( "Gyro FIFO . 0x" + gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_FIFO_CTRL_REG_G ).toString(16) );
-    console.log( "Gyro interrupt source: " + gyroAccelCompass.readReg( IMUClass.LSM9DS0.DEV_GYRO , IMUClass.LSM9DS0.REG_INT1_SRC_G ).toString(16) );
+function showGyrodebugInfo() {
+    console.log("Gyro REG1: 0x" + gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG1_G).toString(16));
+    console.log("Gyro REG2: 0x" + gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG2_G).toString(16));
+    console.log("Gyro REG3: 0x" + gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG3_G).toString(16));
+    console.log("Gyro REG4: 0x" + gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG4_G).toString(16));
+    console.log("Gyro REG5: 0x" + gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_CTRL_REG5_G).toString(16));
+    console.log("Gyro status" + gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_STATUS_REG_G).toString(16));
+    console.log("Gyro FIFO . 0x" + gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_FIFO_CTRL_REG_G).toString(16));
+    console.log("Gyro interrupt source: " + gyroAccelCompass.readReg(IMUClass.LSM9DS0.DEV_GYRO, IMUClass.LSM9DS0.REG_INT1_SRC_G).toString(16));
 }
 
 // ---- UTILITY FUNCTIONS ----------
 function isEmpty(obj) {
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop))
             return false;
     }
 
