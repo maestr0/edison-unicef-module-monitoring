caad9456620877cbc32fd37b015dc833e28ab2c7
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-04-29 13:33:29 -0400

    added python isr.py

---
+++ isr.py
@@ -0,0 +1,30 @@
+import mraa
+import time
+import sys
+
+class Counter:
+    count = 0
+
+c = Counter()
+
+# inside a python interrupt you cannot use 'basic' types so you'll need to use
+# objects
+def test(gpio):
+    print("pin " + repr(gpio.getPin(True)) + " = " + repr(gpio.read()))
+    c.count+=1
+
+pin = 6;
+if (len(sys.argv) == 2):
+    try:
+        pin = int(sys.argv[1], 10)
+    except ValueError:
+        printf("Invalid pin " + sys.argv[1])
+try:
+    x = mraa.Gpio(pin)
+    print("Starting ISR for pin ")
+    x.dir(mraa.DIR_IN)
+    x.isr(mraa.EDGE_BOTH, test, x)
+    var = raw_input("Press ENTER to stop")
+    x.isrExit()
+except ValueError as e:
+    print(e)
