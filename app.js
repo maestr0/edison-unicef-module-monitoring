8568fd0060075e9afa14cd909e2a828b0c09c953
Pawel Raszewski <raszewski@gmail.com>
2016-05-08 17:41:50 -0400

    fixed timeouts

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ rebootCount = process.env.REBOOT_COUNT || "HARDCODED_VALUE";
 
 appMode = process.env.NODE_ENV || "development";
 
+logger("APP MODE " + appMode);
+
 videoDuration = (appMode === "production") ? "40" : "5";
 
 var moduleIsHorizontal = false;
